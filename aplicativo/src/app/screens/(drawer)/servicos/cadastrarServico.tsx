bf194b948d66819e73110032132318c18a0f1cd4
Kayky Ribeiro <kaykykaro04@gmail.com>
2025-01-19 18:01:51 -0300

    assets

--- aplicativo/src/app/screens/(drawer)/servicos/cadastrarServico.tsx
+++ aplicativo/src/app/screens/(drawer)/servicos/cadastrarServico.tsx
@@ -1,7 +1,9 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function CadastarServicos() {
  return (
-   <View/>
+   <View>
+     <Text>Cadastrar Serviço</Text>
+   </View>
   );
 }
--- aplicativo/src/components/addServico.tsx
+++ aplicativo/src/components/addServico.tsx
@@ -7,7 +7,7 @@ export function AddServico() {
   const router = useRouter();
   return (
     <Pressable
-      onPress={() => {router.push('/screens/(drawer)/servicos/cadastrarServico')}} 
+      onPress={() => {router.push('./servicos/cadastrarServico')}} 
       className="w-14 h-14 bg-black rounded-2xl absolute bottom-8 right-8 flex items-center justify-center">
         <AntDesign name="plus" size={40} color="white" />
     </Pressable>
