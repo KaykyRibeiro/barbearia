0cd7b74d3b8aa05d748314598937c18255ea527b
Kayky Ribeiro <kaykykaro04@gmail.com>
2025-02-15 12:07:53 -0300

    designer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,16 +17,40 @@ function App() {
         </div>
       </div>
       <div className="xl:w-4/12 lg:w-6/12 md:w-8/12 sm:w-screen lg:me-10 flex md:justify-center sm:justify-center md:mb-10 ">
-        <div className="xl:w-8/12 w-12/12 h-auto p-5 bg-neutral-950 text-neutral-100">
-          <h2>Rua B. Colonia do Piagui, Guaratinguetá - SP</h2>
-          <div>
-            <p>Whatsapp: (11) 99999-9999</p>
-            <p>Whatsapp: (12) 99999-9999</p>
-            <p>Instagram: @barbearia</p>
-            <p>Facebook: @barbearia</p>
+        <div className="xl:w-8/12 w-12/12 h-auto p-2 bg-neutral-950 text-neutral-100 rounded-3xl">
+          <div className="w-full bg-neutral-300 h-48 rounded-xl mb-4 ">
+            a
+          </div>
+          <h2 className="text-xl font-semibold mx-2">Rua B. Colonia do Piagui, Guaratinguetá - SP</h2>
+          <div className="mt-2 mx-4">
+            <a href="" className="flex flex-row gap-2 items-center mb-2">
+              <img src="./src/assets/whatsapp.png" alt="" className="w-5 h-5" />
+              <p className="text-md font-mono">(11) 99999-9999</p>
+            </a>
+            <a href="" className="flex flex-row gap-2 items-center mb-2">
+              <img src="./src/assets/whatsapp.png" alt="" className="w-5 h-5" />
+              <p className="text-md font-mono">(12) 99999-9999</p>
+            </a>
+            <a href="" className="flex flex-row gap-2 items-center mb-2">
+              <img src="./src/assets/instagram.png" alt="" className="w-5 h-5" />
+              <p className="text-md font-mono">@barbearia</p>
+            </a>
+            <a href="" className="flex flex-row gap-2 items-center mb-2">
+              <img src="./src/assets/facebook.png" alt="" className="w-5 h-5" />
+              <p className="text-md font-mono">@barbearia</p>
+            </a>
+
+            
+            
           </div>
         </div>
 
+      </div>
+      <div className="absolute bottom-0 m-4">
+        <div className="flex flex-row justify-between items-center gap-4">
+          <img src="./src/assets/logo.jpg" alt="" className="w-8 h-8 rounded-full" />
+          <h3 className="text-sm text-neutral-400">© 2025 - Todos os direitos reservados</h3>
+        </div>
       </div>
     </div>
   )
--- web/src/components/Form.tsx
+++ web/src/components/Form.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 const Form = () => {
+  // Obtém a data atual no formato YYYY-MM-DD
+  const today = new Date().toISOString().split("T")[0];
+
+  // Estado para armazenar a data selecionada
+  const [selectedDate, setSelectedDate] = useState(today);
+
   return (
     <form className="flex flex-col">
       <div className="flex md:flex-row flex-col md:justify-between items-center gap-5 xl:gap-0  mb-5">
         <div className="flex flex-col">
           <label className="text-xs font-medium">Nome:</label>
-          <input type="text" placeholder="Digite seu nome" className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-52"/>
+          <input type="text" placeholder="Digite seu nome" className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-52" />
         </div>
         <div className="flex flex-col">
           <label className="text-xs font-medium">Telefone:</label>
@@ -31,12 +38,25 @@ const Form = () => {
       </div>
       <div className="flex md:flex-row flex-col md:justify-between items-center gap-5 xl:gap-2  mb-5">
         <div className="flex flex-col">
+          <label className="text-xs font-medium">Data:</label>
+          <input
+            type="date"
+            className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-56"
+            value={selectedDate} // Define o valor padrão como a data de hoje
+            min={today} // Bloqueia a seleção de datas anteriores
+            onChange={(e) => setSelectedDate(e.target.value)} 
+          />
+        </div>
+
+        <div className="flex flex-col">
           <label className="text-xs font-medium">Horário:</label>
-          <select name="serviço" id="servico" required  className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-56">
+          <select name="serviço" id="servico" required className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-56">
             <option value="">Selecione o horario</option>
-            <option value="cabelo">Corte de cabelo</option>
+            <option value="11.00">11:00</option>
           </select>
         </div>
+      </div>
+      <div className="flex md:flex-row flex-col md:justify-between items-center gap-5 xl:gap-2  mb-5">
         <div className="flex flex-col">
           <label className="text-xs font-medium">Pagamento:</label>
           <select name="serviço" id="servico" required className="text-md bg-neutral-300 py-1 px-3 rounded-xl w-56">
@@ -47,12 +67,13 @@ const Form = () => {
             <option value="credito">Cartão de Crédito</option>
           </select>
         </div>
-      </div>
-      <div className="flex justify-end pr-10 my-5">
-        <label className="text-sm font-bold">Valor: <span className="text-2xl text-green-800">R$ 50,00</span></label>
+        <div className="flex justify-end pr-10">
+          <label className="text-sm font-bold">Valor: <span className="text-2xl text-green-800">R$ 50,00</span></label>
+        </div>
       </div>
 
-      <button className="bg-lime-600 text-neutral-100 text-xl font-bold p-2 rounded mx-10 mt-5">Agendar</button>
+
+      <button className="bg-lime-600 text-neutral-100 text-xl font-bold p-2 rounded mx-10 mt-10">Agendar</button>
     </form>
   )
 }
