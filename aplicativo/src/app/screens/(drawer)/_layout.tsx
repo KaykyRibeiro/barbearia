e02eed94bae9e544128b8793cbb571fbb289d5c6
Kayky Ribeiro <kaykykaro04@gmail.com>
2025-03-01 10:38:29 -0300

    estatistica

--- aplicativo/src/app/screens/(drawer)/_layout.tsx
+++ aplicativo/src/app/screens/(drawer)/_layout.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Alert, Text, View, TouchableOpacity, Pressable } from "react-native";
+import { Text, View, TouchableOpacity, Pressable } from "react-native";
 import { Drawer } from "expo-router/drawer";
 import { Avatar } from "@/src/components/avatar";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { ScrollView } from "react-native-gesture-handler";
 
+import { useRouter } from "expo-router";
+
 export default function Layout() {
+  const router = useRouter();
   return (
     <Drawer
       screenOptions={{
@@ -21,10 +28,10 @@ export default function Layout() {
           {/* Botão para fechar o Drawer */}
           <View className="flex justify-start items-start m-4">
             <TouchableOpacity onPress={() => props.navigation.closeDrawer()}>
-              <AntDesign name="close" size={24} color="white" />
+              <Ionicons name="arrow-forward-circle" size={36} color="white" />
             </TouchableOpacity>
           </View>
-          <ScrollView>
+          <ScrollView className="flex-1 bg">
             <View className="flex-col justify-center items-center mt-4 border-b border-white pb-4">
               <Avatar
                 source={{ uri: "https://github.com/KaykyRibeiro.png" }}
@@ -36,7 +43,28 @@ export default function Layout() {
                 <Text className="text-black font-semibold">Editar Perfil</Text>
               </Pressable>
             </View>
+            <View className="flex-col justify-center">
+              <Pressable className="flex-row gap-2 items-center px-4 py-4 m-1 bg-neutral-800/50">
+                <Ionicons name="people-outline" size={22} color="white" />
+                <Text className="text-white font-semibold text-lg">Area de membros</Text>
+              </Pressable>
+              <View className="flex-1 h-[1px] bg-neutral-700 dark:bg-neutral-100" />
+              <Pressable 
+                className="flex-row gap-2 items-center px-4 py-4 m-1 bg-neutral-800/50"
+                onPress={() => {router.push("./gerencia/estatistica")}}
+                >
+                <MaterialCommunityIcons name="google-analytics" size={22} color="white" />
+                <Text className="text-white font-semibold text-lg">Estatisticas</Text>
+              </Pressable>
+              <View className="flex-1 h-[1px] bg-neutral-700 dark:bg-neutral-100" />
+
+            </View>
+
           </ScrollView>
+          <Pressable className="flex-row gap-2 items-center mt-10 bg-red-300/10  px-2 py-4 absolute bottom-10 right-0 w-full">
+            <FontAwesome6 name="arrow-right-from-bracket" size={22} color="red" />
+            <Text className="text-red-300 font-semibold text-lg">Desconectar</Text>
+          </Pressable>
         </View>
       )}
     />
---
+++ aplicativo/src/app/screens/(drawer)/gerencia/estatistica.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Estatistica() {
+    return (
+        <View>
+            <View>
+                <Text>Estatistica</Text>
+            </View>
+        </View>
+    );
+}
